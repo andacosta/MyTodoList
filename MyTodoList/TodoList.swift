58872e1b46f7d36bd899c115bcf077225fddd10f
Sergio Acosta <and_acosta@hotmail.com>
2015-08-11 16:24:26 -0500

    .

--- MyTodoList/TodoList.swift
+++ MyTodoList/TodoList.swift
@@ -9,5 +9,21 @@
 import UIKit
 
 class TodoList: NSObject {
+	var items: [String] = []
+	func addItem(item: String) {
+		items.append(item)
+	}
+}
 
+extension TodoList: UITableViewDataSource {
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return items.count
+	}
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+		let item = items[indexPath.row]
+		
+		cell.textLabel!.text = item
+		return cell
+	}
 }
